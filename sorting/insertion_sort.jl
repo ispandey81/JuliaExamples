0e16ceb0d0a5445acd8e23206e50686f377344d0
indra <indra.pandey@transport.wa.gov.au>
2020-08-18 12:27:33 +0800

    Adding sorting algorithms
    insertion sort

---
+++ sorting/insertion_sort.jl
@@ -0,0 +1,20 @@
+# sample array
+example_array = [200, 156, 40, 0 , 1, -76, 35]
+
+# this is a mutating function (by convention we use ! at the end of mutating function's name)
+function insertionSort!(arr)
+    for i in 1:length(arr)
+        key = arr[i]
+        j = i - 1
+        while j >= 1 && key < arr[j] 
+            arr[j + 1] = arr[j] 
+            j -= 1
+            arr[j + 1] = key
+        end
+    end
+end
+
+# calling the function
+insertionSort!(example_array)
+# printing the modified array
+example_array
